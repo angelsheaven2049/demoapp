937fd42850b6a7504c39cfcbaf8c3d1f7f180992
Quan Nguyen <quan.nguyen1409@gmail.com>
2019-03-24 22:13:44 +1100

    refactor code

--- app/src/main/java/com/angelsheaven/teremdemoapp/data/network/RemoteContract.kt
+++ app/src/main/java/com/angelsheaven/teremdemoapp/data/network/RemoteContract.kt
@@ -2,10 +2,13 @@ package com.angelsheaven.teremdemoapp.data.network
 
 const val BASE_URL = "https://hacker-news.firebaseio.com/v0/"
 
+const val NEWS_ID = "id"
+
 const val PRINT_FORMAT = "print"
 
 const val PRETTY_FORMAT_TYPE = "pretty"
 
 const val GET_TOP_STORIES = "topstories.json"
 
-const val GET_NEWS_DETAIL = "item/{id}"
+const val GET_NEWS_DETAIL = "item/{$NEWS_ID}"
+
